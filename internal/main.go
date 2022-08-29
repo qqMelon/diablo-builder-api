a068da935864ef748741f48e1ca6baecb62977bd
qqMelon <urie.zahard@gmail.com>
2022-08-29 10:01:36 +0200

    Model structured template init

--- internal/main.go
+++ internal/main.go
@@ -2,21 +2,65 @@ package main
 
 import (
 	"fmt"
-	"html"
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/qqMelon/diablo-builder-api/pkg/swagger/server/restapi"
+	"github.com/qqMelon/diablo-builder-api/pkg/swagger/server/restapi/operations"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-		if err != nil {
-			return
+	// Init Swagger
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	api := operations.NewDiabloBuilderAPIAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+
+	defer func() {
+		if err := server.Shutdown(); err != nil {
+			// Error handler
+			log.Fatal(err)
 		}
-	})
+	}()
 
-	log.Println("Listening on localhost:8080")
+	server.Port = 8080
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
+	api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHelloUser)
+	api.GetGopherNameHandler = operations.GetGopherNameHandlerFunc(GetGopherByName)
+
+}
+
+// Health route return Status
+func Health(operations.CheckHealthParams) middleware.Responder {
+	return operations.NewCheckHealthOK().WithPayload("OK")
+}
+
+// GetHelloUser return Hello + your name
+func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
+	return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + " !")
+}
+
+// GetGopherByName return gopher in png
+func GetGopherByName(gopher operations.GetGopherNameParams) middleware.Responder {
+	var URL string
+
+	if gopher.Name != "" {
+		URL = "https://github.com/scraly/gophers/raw/main/" + gopher.Name + ".png"
+	} else {
+		// Default return
+		URL = "https://github.com/scraly/gophers/raw/main/dr-who.png"
+	}
+
+	response, err := http.Get(URL)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return operations.NewGetGopherNameOK().WithPayload(response.Body)
 }
--- pkg/swagger/gen.go
+++ pkg/swagger/gen.go
@@ -1,1 +1,5 @@
 package swagger
+
+//go:generate rm -rf server
+//go:generate mkdir -p server
+//go:generate swagger generate server --quiet --target server --name diablo-builder-api --spec swagger.yml --exclude-main
